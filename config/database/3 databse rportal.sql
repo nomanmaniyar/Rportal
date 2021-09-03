f0d77b0359569daf2b4cc33be58c8900371981eb
unknown <aashutoshmali1460@gmail.com>
2021-09-03 16:19:38 +0530

    main

---
+++ config/database/3 databse rportal.sql
@@ -0,0 +1,63 @@
+CREATE DATABASE IF NOT EXISTS `rportal` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `rportal`;
+
+CREATE TABLE IF NOT EXISTS `secretary` (
+	`Sid` int(11) NOT NULL AUTO_INCREMENT,
+  	`Susername` varchar(50) NOT NULL,
+  	`Spassword` varchar(255) NOT NULL,
+  	`Scode` varchar(30) NOT NULL,
+	`Semail` varchar(100) NOT NULL,
+  	`Sotp` int(15) ,
+  	`Sname` varchar(255) NOT NULL,
+  	`Sflatno` int(20) NOT NULL,
+  	`Swing` varchar(50) NOT NULL,
+  	`Smobile` varchar(255) NOT NULL,
+    PRIMARY KEY (`Sid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+ALTER TABLE secretary ADD CONSTRAINT UNIQUE (semail);
+INSERT INTO `secretary` (`Sid`, `Susername`, `Spassword`,`Scode`, `Semail`,`Sotp`,`Sname`,`Sflatno`,`Swing`,`Smobile`) VALUES (1, 'stest', 'stestpw', 'ZZZZ55', 'jais65142@gmail.com',NULL,'sec test','1001','A','9420829593');
+select * from secretary;
+
+CREATE TABLE IF NOT EXISTS `member` (
+	`Mid` int(11) NOT NULL AUTO_INCREMENT,
+  	`Musername` varchar(50) NOT NULL,
+  	`Mpassword` varchar(255) NOT NULL,
+  	`Mcode` varchar(30) NOT NULL,
+	`Memail` varchar(100) NOT NULL,
+  	`Motp` int(15) ,
+  	`Mname` varchar(255) NOT NULL,
+  	`Mflatno` int(20) NOT NULL,
+  	`Mwing` varchar(50) NOT NULL,
+  	`Mmobile` varchar(255) NOT NULL,
+    PRIMARY KEY (`Mid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+ALTER TABLE member ADD CONSTRAINT UNIQUE (Memail);
+INSERT INTO `member` (`Mid`, `Musername`, `Mpassword`,`Mcode`, `Memail`,`Motp`,`Mname`,`Mflatno`,`Mwing`,`Mmobile`) VALUES ('1', 'mtest', 'mtestpw', 'ZZZZ55', 'aashutoshmali1460@gmail.com',NULL,'mem test','2002','A','9922338265');
+select * from member;
+
+CREATE TABLE IF NOT EXISTS `society` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`code` varchar(30) NOT NULL,
+  	`name` varchar(50) NOT NULL,
+	`city` varchar(50) NOT NULL,
+	`road` varchar(255) NOT NULL,
+  	`area` varchar(30) NOT NULL,
+	`state` varchar(100) NOT NULL,
+  	`pin` int(15) NOT NULL,
+  	`acname` varchar(255) NOT NULL,
+  	`acno` varchar(200) NOT NULL,
+  	`mmid` varchar(50) NOT NULL,
+  	`bankname` varchar(255) NOT NULL,
+    `branch` varchar(200) NOT NULL,
+  	`ifsc` varchar(255) NOT NULL,
+  	`socrule` varchar(2000),
+	`mainrule` varchar(2000),
+    PRIMARY KEY (`id`,`code`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+ALTER TABLE society ADD CONSTRAINT UNIQUE (code);
+INSERT INTO `society` (`id`, `code`, `name`,`city`, `road`,`area`,`state`,`pin`,`acname`,`acno`,`mmid`,`bankname`,`branch`,`ifsc`,`socrule`,`mainrule`) VALUES (1, 'ZZZZ55', 'Gokuldham SOciety', 'Goregaon east', 'powder gali','goregaon','maharastra','422003','bhide op','9922338265','515515','tooti footi bank','usa','aajaus5415',NULL,NULL);
+select * from society;
+
+alter table society auto_increment = 2;
+SELECT * FROM member INNER JOIN society ON member.Mcode = society.code INNER JOIN secretary ON member.Mcode = secretary.Scode;
+SET GLOBAL sql_mode = '';
---
+++ main.py
@@ -0,0 +1,281 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+import math, random
+import random
+from ssl import create_default_context
+from email.message import EmailMessage
+from flask_mail import * 
+from flask import *
+from flask_mail import Message 
+
+app = Flask(__name__)
+app.secret_key = 'otp'
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_DB'] = 'rportal'
+mysql = MySQL(app)
+ 
+mail = Mail(app)
+app.config["MAIL_SERVER"]='smtp.gmail.com'  
+app.config["MAIL_PORT"] = 465 #465 or 587 
+app.config["MAIL_USERNAME"] = 'ajinfotics@gmail.com'  
+app.config['MAIL_PASSWORD'] = 'JAIS@65142'  
+#app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+part2 ="Your One Time Password for logging into R-Portal is\n\n" 
+part3="""\n\nIf you did not Initiated this request,please ignore this mail.
+
+If you have more queries, feel free to contact us at ajinfotics@gmail.com. 
+
+We will love to help and assist you at any movement.
+
+Automated mail sent by R-Portal. Please do not reply.
+Regards! """
+mail = Mail(app)
+otp = random.randint(000000,999999)  
+
+@app.route('/')
+def index():
+        return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM secretary WHERE Susername = %s AND Spassword = %s', (username, password,))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['Sid']
+            session['username'] = account['Susername']
+            return sotp() 
+        elif request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+            username = request.form['username']
+            password = request.form['password']
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('SELECT * FROM member WHERE Musername = %s AND Mpassword = %s', (username, password,))
+            account = cursor.fetchone()
+            if account:
+                session['loggedin'] = True
+                session['id'] = account['Mid']
+                session['username'] = account['Musername']
+                return motp()       
+            else:
+                msg = 'Incorrect username/password! Please check Username/Password and try again!'
+    return render_template('login.html', msg=msg)
+
+def sotp():
+    if 'loggedin' in session:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM secretary WHERE Sid = %s', (session['id'],))
+        account = cursor.fetchone()
+        if account:
+            email = account['Semail']
+            msg = Message('OTP confirmation for RPortal' ,sender ='Rportal<me@Rportal.com', recipients = [email])
+            msg.body = part2 + str(otp)+ part3 
+            mail.send(msg) 
+            return render_template('sotp.html')
+        else:
+                msg = 'Something went wrong:( Please try again!'
+    else:
+        return render_template("index.html")
+def motp():
+    if 'loggedin' in session:
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('SELECT * FROM member WHERE Mid = %s ', (session['id'],))
+            account = cursor.fetchone()
+            if account:
+                email = account['Memail']
+                msg = Message('OTP confirmation for RPortal' ,sender ='Rportal<me@Rportal.com', recipients = [email]) 
+                msg.body = part2 + str(otp) + part3 
+                mail.send(msg)  
+                return render_template('Motp.html')
+            else:
+                msg = 'Something went wrong:( Please try again!'
+    else:
+        return render_template("index.html")
+
+@app.route('/svalidate',methods=["POST"])
+def svalidate():  
+    user_otp = request.form['otp']  
+    if otp == int(user_otp):  
+        return redirect(url_for('shome')) 
+    else: 
+        return render_template("rportal.html")
+
+@app.route('/motpvalidate',methods=["POST"])
+def mvalidate():  
+    user_otp = request.form['otp']  
+    if otp == int(user_otp):  
+        return redirect(url_for('shome')) 
+    else: 
+        return render_template("index.html")  
+
+@app.route('/logout')
+def logout():
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   return redirect(url_for('index'))
+
+def invitation():
+    num = '0123456789'
+    string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    num1 = ""
+    str1 = ""
+    length = len(string)
+    length1 = len(num)
+    for i in range(4):
+        str1 += string[math.floor(random.random() * length)]
+    for i in range(2):
+        num1 += num[math.floor(random.random() * length1)]
+    code = str(str1 + num1)
+    return code
+
+@app.route('/sregister', methods=['GET','POST'])
+def sregister():
+    msg = ''
+    if request.method == 'POST' and 'Sname' in request.form and 'Susername' in request.form and 'Semail' in request.form:
+        
+        username = request.form['Susername']
+        email = request.form['Semail']
+        password = request.form['Spassword']
+        Aname = request.form['Sname']
+        flatno = request.form['Sflatno']
+        wing = request.form['Swing']
+        mobile = request.form['Smobile'] 
+        acname = request.form['acname']  
+        acno = request.form['acno']
+        mmid = request.form['mmid']
+        bankname = request.form['bankname']
+        branch = request.form['branch']
+        ifsc = request.form['ifsc']
+        name = request.form['name']
+        area = request.form['area']
+        road = request.form['road']   
+        city = request.form['city']
+        state = request.form['state']
+        pin = request.form['pin']
+        code = ""
+        code =invitation()
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM member WHERE Musername = %s AND Memail = %s', (username, email))
+        account = cursor.fetchone()
+        cursor1 = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor1.execute('SELECT * FROM secretary WHERE Susername = %s AND Semail = %s', (username, email))
+        account1 = cursor1.fetchone()
+        cursor2 = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor2.execute('SELECT * FROM society WHERE code = %s', (code,))
+        account2 = cursor2.fetchone()
+        if account:
+            msg = 'Warning! Account already exists!!'
+        elif account1:
+            msg = 'Warning! Account already exists!!'
+        elif account2:
+            msg = 'Something went wrong:( Please try again!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            cursor1.execute('INSERT INTO secretary VALUES (NULL, %s, %s, %s, %s, NULL, %s, %s, %s,%s)', (username , password , code ,  email , Aname , flatno , wing , mobile))
+            cursor2.execute('INSERT INTO society VALUES (NULL, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, NULL, NULL)', (code , name , city , road , area , state , pin , acname, acno, mmid, bankname, branch, ifsc))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+    elif request.method == 'POST':
+        msg = 'elif code!' 
+    return render_template('sregister.html', msg=msg)
+
+@app.route('/shome')
+def shome():
+    if 'loggedin' in session:
+        return render_template('shome.html', Susername=session['username'])
+    return redirect(url_for('login'))
+
+@app.route('/sprofile')
+def sprofile():
+    if 'loggedin' in session:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM secretary WHERE Sid = %s', (session['id'],))
+        account = cursor.fetchone()
+        return render_template('sprofile.html', account=account)
+    return redirect(url_for('login'))
+
+@app.route('/mcode', methods=['GET', 'POST'])
+def mcode():
+    msg = ''
+    if request.method == 'POST' and 'code' in request.form:
+        code = request.form['code']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM society WHERE code = %s', (code,))
+        account = cursor.fetchone()
+        if account:
+            cursor.execute('select name, city, road, area, state, pin from society WHERE code = %s', (code ,))
+            account = cursor.fetchone()
+            return render_template('mverify.html', account=account)
+        else:
+            mysql.connection.commit()
+            msg='Invalid Society Code!'
+    return render_template('mcode.html', msg=msg)
+
+@app.route('/mregister', methods=['GET', 'POST'])
+def mregister():
+    msg = ''
+    if request.method == 'POST' and 'Musername' in request.form and 'Mpassword' in request.form and 'Memail' in request.form and 'Mcode' in request.form and 'Mname' in request.form and 'Mflatno' in request.form and 'Mwing' in request.form and 'Mmobile' in request.form:
+        username = request.form['Musername']
+        password = request.form['Mpassword']
+        code = request.form['Mcode']
+        email = request.form['Memail']
+        name = request.form['Mname']
+        flatno = request.form['Mflatno']
+        wing = request.form['Mwing']
+        mobile = request.form['Mmobile']   
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM member WHERE Musername = %s AND Memail = %s', (username, email))
+        account = cursor.fetchone()
+        cursor1 = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor1.execute('SELECT * FROM secretary WHERE Susername = %s AND Semail = %s', (username, email))
+        account1 = cursor1.fetchone()
+        if account:
+            msg = 'Warning! Account already exists!!'
+        elif account1:
+            msg = 'Warning! Account already exists!!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            cursor.execute('INSERT INTO member VALUES (NULL, %s, %s, %s, %s, NULL, %s, %s, %s,%s)', (username , password , code ,  email , name , flatno , wing , mobile))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form!'
+    return render_template('mregister.html', msg=msg)
+
+@app.route('/mhome')
+def mhome():
+    if 'loggedin' in session:
+        return render_template('mhome.html', Musername=session['username'])
+    return redirect(url_for('login'))
+
+@app.route('/mprofile')
+def mprofile():
+    if 'loggedin' in session:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM member WHERE Mid = %s', (session['id'],))
+        account = cursor.fetchone()
+        return render_template('mprofile.html', account=account)
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+   app.run(debug=True)
