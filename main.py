a1772d45a5db8659f0792fff766b6902060dfdca
unknown <aashutoshmali1460@gmail.com>
2021-11-20 17:46:38 +0530

    meet

--- main.py
+++ main.py
@@ -15,10 +15,14 @@ from flask_mail import Message
 import os 
 import hashlib
 import sys
+import jwt
+import requests
+import json
+from time import time
 #sys.path.insert(0, 'Rportal/config')
 #from config import credentials as cred
-
-
+API_KEY = 'DVKW9UngTg2lI1FnqrQVWA'
+API_SEC = 'ZIN4AsaniiKYBBA0cQHLSupJOIZwMEdbcRm2'
 UPLOAD_FOLDER = '/path/to/the/uploads'
 ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
 
@@ -1022,10 +1026,6 @@ def staff_profile():
     else:
         return logout()
 
-#
-#   Other Modules
-#
-
 #   Contact Us
 @app.route('/R-Portal/contactus', methods=['GET', 'POST'] )
 def contactus():
@@ -1038,8 +1038,76 @@ def contactus():
         mysql.connection.commit()
         return render_template('rportal.html')
 
-#   Module Name Here
+#   
+#Zoom meet JWT
+#@app.route('/R-Portal/createmeeting', methods=['GET', 'POST'] )
+# create a function to generate a token 
+# using the pyjwt library
+def generateToken():
+ #if 'secretary' in session:
+    token = jwt.encode(
+        
+        # Create a payload of the token containing 
+        # API Key & expiration time
+        {'iss': API_KEY, 'exp': time() + 5000},
+          
+        # Secret used to generate token signature
+        API_SEC,
+          
+        # Specify the hashing alg
+        algorithm='HS256'
+    )
+    
+    return token
+       
+  
+  
+# create json data for post requests
+meetingdetails = {"topic": "The title of your zoom meeting",
+                  "type": 2,
+                  "start_time": "2019-06-14T10: 21: 57",
+                  "duration": "45",
+                  "timezone": "Europe/Madrid",
+                  "agenda": "test",
+  
+                  "recurrence": {"type": 1,
+                                 "repeat_interval": 1
+                                 },
+                  "settings": {"host_video": "true",
+                               "participant_video": "true",
+                               "join_before_host": "False",
+                               "mute_upon_entry": "False",
+                               "watermark": "true",
+                               "audio": "voip",
+                               "auto_recording": "cloud"
+                               }
+                  }
+   
 
+  
+# send a request with headers including 
+# a token and meeting details
+def createMeeting():
+    headers = {'authorization': 'Bearer %s' % generateToken(),
+               'content-type': 'application/json'}
+    r = requests.post(
+        f'https://api.zoom.us/v2/users/me/meetings', 
+      headers=headers, data=json.dumps(meetingdetails))
+  
+    print("\n creating zoom meeting ... \n")
+    # print(r.text)
+    # converting the output into json and extracting the details
+    y = json.loads(r.text)
+    join_URL = y["join_url"]
+    meetingPassword = y["password"]
+  
+    print(
+        f'\n here is your zoom meeting link {join_URL} and your \
+        password: "{meetingPassword}"\n')
+  
+  
+# run the create meeting function
+createMeeting()
 
 #
 #   Runner Code
